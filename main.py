de4c3fa88374e962708fb63faee1b13155707e31
Chhaya Kumar Das <chayakudas.das56@gmail.com>
2023-12-26 12:17:56 +0530

    Adding pytorch llama from scratch implementation

---
+++ main.py
@@ -0,0 +1,39 @@
+import torch
+from inference import LLaMA
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    prompts = [
+        "Simply put, the theory of relativity states that ",
+        "If Google was an Italian company founded in Milan, it would",
+        # Few shot promt
+        """Translate English to French:
+
+        sea otter => loutre de mer
+        peppermint => menthe poivrée
+        plush girafe => girafe peluche
+        cheese =>""",
+        # Zero shot prompt
+        """Tell me if the following person is actually Doraemon disguised as human:
+        Name: Umar Jamil
+        Decision: 
+        """
+    ]
+
+    model = LLaMA.build(
+        checkpoints_dir='llama-2-7b-chat/',
+        tokenizer_path='tokenizer.model',
+        load_model=True,
+        max_seq_len=1024,
+        max_batch_size=len(prompts),
+        device=device
+    )
+
+    out_tokens, out_texts = (model.text_completion(prompts, max_gen_len=64, device="cpu"))
+    assert len(out_texts) == len(prompts)
+    for i in range(len(out_texts)):
+        print(f'{out_texts[i]}')
+        print('-' * 50)
+
+
